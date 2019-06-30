640388d1d365e906770b85a4595e830498e0f9a0
Leopold Kwok <leopoldkwok@gmail.com>
2019-06-30 10:04:34 +0100

    ES6 import/export

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,47 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('app.js is running');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
+// import './utils.js';
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+// person.js
+// named export isAdult(18) - true if adult, otherwise failed
+// named export canDrink(18) - true if 21 and over, otherwise false
+
+// import isAdult and canDrink
+// use both printing result to the console
+
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](17));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](21));
+
+
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+console.log('person.js is running');
+
+const isAdult = (age) => age >=18;
+const canDrink = (age) => age >= 21;
+
+
+
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('app.js is running');
+// import './utils.js';
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+// person.js
+// named export isAdult(18) - true if adult, otherwise failed
+// named export canDrink(18) - true if 21 and over, otherwise false
+
+// import isAdult and canDrink
+// use both printing result to the console
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(17));
+console.log(canDrink(21));
+
